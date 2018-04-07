ce52d1b7d4799215933ddcc03d320dc54c1ec4ed
Virva <virva.svala@gmail.com>
2018-04-07 09:52:20 +0300

    creating domain package and classes

---
+++ FitMe/src/main/java/fitme/domain/Diary.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fitme.domain;
+    
+/**
+ * Yksittäistä  ruokalajia kuvaava luokka 
+ */
+
+/**
+ *
+ * @author svsv
+ */
+public class Diary {
+
+
+    private int id;  //päivämäärä? idksi?
+    private String content;
+    private boolean delete;  //done korvasin delete
+    private User user;
+
+    public Diary(int id, String content, boolean delete, User user) {
+        this.id = id;
+        this.content = content;
+        this.delete = delete;
+        this.user = user;
+    }
+    
+    public Diary(String content, User user) {
+        this.content = content;
+        this.user = user;
+        this.delete = false;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isDelede() {// isDone() {
+        return delete;
+    }
+
+    public void setDelete() {
+        delete = true;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Diary)) {
+            return false;
+        }
+        Diary other = (Diary) obj;
+        return id == other.id;
+    }
+
+}
---
+++ FitMe/src/main/java/fitme/domain/User.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fitme.domain;
+
+/**
+ *
+ * @author svsv
+
+ * Järjestelmän käyttäjää edustava luokka 
+ */
+public class User {
+    
+
+    private String name;
+    private String username;
+
+    public User(String username, String name) {
+        this.name = name;
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        
+        User other = (User) obj;
+        return username.equals(other.username);
+    }
+    
+}
